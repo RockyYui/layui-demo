0883046088c1f0c224d9b84bec96f8c26eddbdd4
RockyYui <490129667@qq.com>
2017-01-20 17:14:31 +0800

    '新增文件'

--- js/modules/index.js
+++ js/modules/index.js
@@ -1,12 +1,14 @@
 /**
  * Created by Rocky Yui on 2017/1/20 0020.
  */
-layui.define(['layer','form'],function (exports) {
-    var layer = layui.layer,
+layui.define(['jquery','layer','form'],function (exports) {
+    var $ = layui.jquery,
+        layer = layui.layer,
         form = layui.form();
 
     console.log(layui.device());
     console.log(layui.cache);
+    console.log($);
 
     var indexObj = {
         ok:function (text) {
