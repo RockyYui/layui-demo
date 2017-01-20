2bed3d6b8aae66d498f1cced13cf4ac1c02d1bde
RockyYui <490129667@qq.com>
2017-01-20 16:19:33 +0800

    默认的

---
+++ js/modules/index.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Rocky Yui on 2017/1/20 0020.
+ */
+layui.define(['layer','form'],function (exports) {
+    var Layer = layui.layer,
+        Form = layui.form();
+
+    console.log(layui.device());
+    console.log(layui.cache);
+
+    exports("index",{});
+})
